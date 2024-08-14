48abc28da8492bb350a525fff56120adbb69d507
Basker <bg_basker@m1.com.sg>
2024-08-14 12:39:58 +0530

    Redux Provider

--- app/GlobalRedux/provider.tsx
+++ app/GlobalRedux/provider.tsx
@@ -5,7 +5,11 @@ import { store } from "./store";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
-export function Providers({ children }) {
+export function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   let persistor = persistStore(store);
   return (
     <Provider store={store}>
