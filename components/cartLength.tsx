20dd4f796de1e96d10d115cbf1a460b918f9df0e
Basker <bg_basker@m1.com.sg>
2024-08-18 13:50:11 +0530

    Cart Store, Redis updates1

--- components/cartLength.tsx
+++ components/cartLength.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function CartLength() {
-  const cartItems = useSelector((state) => state.cart);
+  const cartItems = useSelector((state: any) => state.cart);
   const cartLength = cartItems.length;
   return <span>{cartLength}</span>;
 }
