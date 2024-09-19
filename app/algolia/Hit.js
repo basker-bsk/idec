2cbb1de1a1016af4644b43f5e4d86b439f6aab86
Ganesh kumar R <bg_ganesh@m1.com.sg>
2024-09-19 10:33:35 +0530

    algolia categories filters added

--- app/algolia/Hit.js
+++ app/algolia/Hit.js
@@ -1,9 +1,9 @@
 "use client";
-
+import { Highlight } from "react-instantsearch";
 export default function Hit({ hit }) {
   return (
     <>
-      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5">
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5">
         <h2>{hit.product_name}</h2>
         <a href="#" className="block mt-5">
           <img
@@ -14,15 +14,15 @@ export default function Hit({ hit }) {
             className="border p-2 border-gray-300 bg-white rounded-md w-auto h-auto"
           />
         </a>
-        <div class="p-5">
+        <div className="p-5">
           <a href="#">
-            <h5 class="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">
               {hit.product_code}
             </h5>
           </a>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {hit.product_description}
-          </p>
+          <div className="price">{hit.price}</div>
+
+          <Highlight attribute="product_description" hit={hit} />
         </div>
       </div>
     </>
--- app/algolia/page.js
+++ app/algolia/page.js
@@ -1,7 +1,16 @@
 "use client";
 // Importing modules
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Stats,
+  SortBy,
+  Pagination,
+  RefinementList,
+  Menu,
+} from "react-instantsearch";
 import Hit from "./Hit";
 import "./search.css";
 const searchClient = algoliasearch(
@@ -15,9 +24,28 @@ export default function SearchBar() {
       <InstantSearch searchClient={searchClient} indexName="algolia_data">
         {/* Adding Search Box */}
         <SearchBox translations={{ placeholder: "Search for products" }} />
-
-        {/* Adding Data */}
-        <Hits hitComponent={Hit} />
+        <div className="filter-group">
+          <Stats />
+          <SortBy
+            defaultRefinement="algolia_data"
+            items={[
+              { value: "algolia_data", label: "Most Relevant" },
+              { value: "algolia_data_price_asc", label: "Lowest Price" },
+              { value: "algolia_data_price_desc", label: "Highest Price" },
+            ]}
+          />
+        </div>
+        <div className="content_block">
+          <div className="category_block">
+            <h2>Categories</h2>
+            <RefinementList attribute="category" />
+          </div>
+          <div className="hits_block">
+            {/* Adding Data */}
+            <Hits hitComponent={Hit} />
+            <Pagination showLast />
+          </div>
+        </div>
       </InstantSearch>
     </>
   );
