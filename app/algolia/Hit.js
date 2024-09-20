29cc9fd7a12fa19a06f748c7ac1d6e27ef30e070
Basker <bg_basker@m1.com.sg>
2024-09-20 12:40:23 +0530

    Algolia UI update

--- app/algolia/Hit.js
+++ app/algolia/Hit.js
@@ -5,7 +5,7 @@ export default function Hit({ hit }) {
     <>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5 relative">
         <h2 className="prod_title">{hit.product_name}</h2>
-        <a href="#" className="block mt-5">
+        <a href="#" className="block mt-2">
           <img
             src={hit.product_image}
             width={550}
@@ -14,13 +14,13 @@ export default function Hit({ hit }) {
             className="border p-2 border-gray-300 bg-white rounded-md w-auto h-auto"
           />
         </a>
-        <div className="p-5">
+        <div className="py-4">
           <a href="#">
             <h5 className="brandname">{hit.brand}</h5>
           </a>
           <div className="price">&#8377; {hit.price}</div>
 
-          <Highlight attribute="product_description" hit={hit} />
+          <Highlight className="text-[14px]" attribute="product_description" hit={hit} />
           {hit.free_shipping ? (
             <div className="free_wrap">
               <div className="free">Free Shipping</div>
