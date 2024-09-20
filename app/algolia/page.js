3187037004af46e6bf9bb5132eb45856e499bd7b
Basker <bg_basker@m1.com.sg>
2024-09-20 14:45:28 +0530

    Algolia UI update2

--- app/algolia/page.js
+++ app/algolia/page.js
@@ -23,30 +23,38 @@ export default function SearchBar() {
     <>
       <InstantSearch searchClient={searchClient} indexName="algolia_data">
         {/* Adding Search Box */}
-        <SearchBox translations={{ placeholder: "Search for products" }} />
-        <div className="filter-group">
-          <Stats />
-          <SortBy
-            defaultRefinement="algolia_data"
-            items={[
-              { value: "algolia_data", label: "Most Relevant" },
-              { value: "algolia_data_price_asc", label: "Lowest Price" },
-              { value: "algolia_data_price_desc", label: "Highest Price" },
-            ]}
-          />
+        <div className="flex items-end my-8 justify-between w-full gap-4">
+          <div className="w-1/4">&nbsp;</div>
+          <div className="w-full lg:w-3/4 flex justify-between px-3">
+            <div className="flex  flex-col">
+              <SearchBox translations={{ placeholder: "Search for products" }} className="" />
+              <Stats />
+            </div>
+            <div className="filter-group flex items-end">          
+              <SortBy
+                defaultRefinement="algolia_data"
+                items={[
+                  { value: "algolia_data", label: "Most Relevant" },
+                  { value: "algolia_data_price_asc", label: "Lowest Price" },
+                  { value: "algolia_data_price_desc", label: "Highest Price" },
+                ]}
+              />         
+            </div>
+          </div>
         </div>
-        <div className="content_block">
-          <div className="category_block">
-            <h2>Brands</h2>
+       
+        <div className="flex gap-4">
+          <div className="w-full lg:w-1/4 flex gap-4 flex-col">
+            <h2 className="text-sm">Brands</h2>
             <RefinementList attribute="brand" />
-            <h2>Categories</h2>
+            <h2 className="text-sm">Categories</h2>
             <RefinementList attribute="category" />
-            <h2>Price</h2>
+            <h2 className="text-sm">Price</h2>
             <RefinementList attribute="price" />
-            <h2>Free Shipping</h2>
+            <h2 className="text-sm">Free Shipping</h2>
             <RefinementList attribute="free_shipping" />
           </div>
-          <div className="hits_block">
+          <div className="w-full lg:w-3/4">
             {/* Adding Data */}
             <Hits hitComponent={Hit} />
             <Pagination showLast />
