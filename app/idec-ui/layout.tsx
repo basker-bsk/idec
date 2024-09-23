a44039a975ff748c07af81a8c88d0d7faa611c47
Basker <bg_basker@m1.com.sg>
2024-09-23 09:16:08 +0530

    ui updates2

--- app/idec-ui/layout.tsx
+++ app/idec-ui/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <div className="hidden lg:flex">
           <Sidebar />
         </div>
-        <main className="py-[10px] pl-[10px] lg:pl-[320px] pr-[10px] lg:pr-[20px] w-full lg:w-auto lg:py-[20px] overflow-y-auto">
+        <main className="py-[10px] pl-[10px] lg:pl-[320px] pr-[10px] lg:pr-[20px] w-full lg:py-[20px] overflow-y-auto">
           <div className="mb-4 flex gap-2 lg:hidden items-center justify-center">
             <Header />
           </div>
