17662e27e715a999bd6bc6d0c3420440b893d5b3
Basker <bg_basker@m1.com.sg>
2024-09-23 09:29:37 +0530

    ui updates3

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -28,12 +28,12 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 text-left font-medium transition-all [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
-      {children}
+      <span>{children}</span>
       <ChevronDown className="h-8 w-8 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
