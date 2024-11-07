e4778e708ea828b3a258dfc578611ed05d4f3755
Ganesh kumar R <gr00869034@techmahindra.com>
2024-11-07 12:40:39 +0530

    updated clp and slp pages

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,34 +1,112 @@
 "use client";
 import Image from "next/image";
-import { Button } from "../ui/button";
-function ProductCard() {
-  return (
-    <div className="max-w-[323px] ml-14 rounded-2xl border-halfGray border bg-white px-5">
-      <div className="w-full flex justify-center py-10">
-        <Image
-          src="/assets/images/switch.png"
-          width={180}
-          height={180}
-          alt=""
-          className="object-cover"
-        />
-      </div>
-      <div className="py-6">
-        <div className="font-medium text-20 leading-7 text-black">
-          Pushbuttons and Pilot Lights (23)
-        </div>
-        <p className="text-16 leading-6 mt-2 text-black">
-          IDEC switches and pilot lights are the best engineered switches in the
-          world for a reason.
-        </p>
-      </div>
-      <div className="flex justify-start">
-        <Button theme="secondary" variant="iconText">
-          View All Series <span className="icon-chevron-right"></span>
-        </Button>
-      </div>
-    </div>
-  );
+import { Button } from "@/components/ui/button";
+import React from "react";
+import Link from "next/link";
+import classNames from "classnames";
+import ScreenSize from "@/utils/ScreenSize";
+interface ProductCardProps {
+  data: any;
 }
+const ProductCard = ({ data }: ProductCardProps) => {
+  console.log("card props item", data);
+  const cardType: string = data?.cdStyle;
+  const ctaText: string = data?.cdLink?.linkText;
+  const ctaLink: string = data?.cdLink?.linkUrl;
+  const description: string = data?.cdDescription;
+  const title: string = data?.cdTitle;
+  const imageSrc: string = data?.cdImage?.url;
+  const imageAlt: string = data?.cdImage?.title;
+  const price: string = data?.price;
+  const descriptionList: [] = data?.descriptionList;
+  const styleTypeA: string = "category-landing";
+  const styleTypeB: string = "series-landing";
+  const styleTypeC: string = "series-listing";
+  return (
+    <>
+      <div
+        className={classNames(
+          "w-full lg:max-w-[323px] rounded-2xl border-halfGray border bg-white",
+          { "p-[30px]": cardType === styleTypeB },
+          { "px-4 lg:px-5": cardType === styleTypeA },
+          { "px-3 lg:px-5": cardType === styleTypeC }
+        )}
+      >
+        {cardType === styleTypeB && (
+          <div className="text-16 lg:text-20 font-medium leading-[150%] text-black lg:px-[15px] text-center">
+            {title}
+          </div>
+        )}
+        <div
+          className={classNames(
+            "w-full flex justify-center items-center",
+            { "mt-[18px] lg:mt-4": cardType === styleTypeB },
+            { "py-6 lg:py-10": cardType === styleTypeA },
+            { "pt-[26px] lg:py-10": cardType === styleTypeC }
+          )}
+        >
+          <Image
+            src={imageSrc}
+            width={ScreenSize() ? (cardType === styleTypeB ? 160 : 180) : 120}
+            height={ScreenSize() ? (cardType === styleTypeB ? 160 : 180) : 120}
+            alt={imageAlt}
+            className="object-cover"
+          />
+        </div>
+        {cardType === styleTypeA && (
+          <div className="pb-4 lg:py-5">
+            <div className="font-medium text-16 lg:text-20 leading-[150%] text-black">
+              {title}
+            </div>
+            <p className="text-14 lg:text-16 leading-[150%] lg:leading-6 mt-2 text-black">
+              {description}
+            </p>
+            <div className="flex justify-start mt-6">
+              <Button theme="secondary" variant="iconText">
+                <Link href={ctaLink}>{ctaText}</Link>
+                <span className="icon-chevron-right"></span>
+              </Button>
+            </div>
+          </div>
+        )}
+        {cardType === styleTypeB && (
+          <div className="flex justify-center mt-[18px] lg:mt-6">
+            <Button theme="secondary" variant="iconText">
+              <Link href={ctaLink}>{ctaText}</Link>
+              <span className="icon-chevron-right"></span>
+            </Button>
+          </div>
+        )}
+        {cardType === styleTypeC && (
+          <div className="pt-4 pb-3 lg:pt-5 lg:pb-5">
+            <div className="lg:font-medium text-12 lg:text-14 leading-[150%] text-gray-600">
+              {description}
+            </div>
+            <div className="font-medium text-16 lg:text-18 leading-[150%] text-black mt-1 min-h-[47px] lg:min-h-[55px]">
+              {title}
+            </div>
+            <div className="text-18 lg:text-20 font-bold text-secondary leading-[150%] mt-2 lg:mt-5">
+              ${price}{" "}
+              <span className="uppercase text-12 lg:text-14 font-medium leading-[150%] text-gray-400">
+                MSRP
+              </span>
+            </div>
+            <ul className="text-gray-600 text-10 lg:text-12 leading-[160%] lg:leading-4 list-disc pl-5 mt-[6px] lg:mt-2">
+              {descriptionList &&
+                descriptionList.map((list, i) => {
+                  return <li key={i}>{list}</li>;
+                })}
+            </ul>
+            <div className="flex justify-start mt-3 lg:mt-6">
+              <Button theme="secondary" className="w-full">
+                {ctaText}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default ProductCard;
